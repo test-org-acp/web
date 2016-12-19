b0b306f7e2dba50c00354f2738d74f5bc25de9e1
testaceproject <komatsu.yura.870@m.kyushu-u.ac.jp>
2016-12-19 16:42:31 +0900

    webpage

---
+++ main/en/05_downloads/header_bootstrap.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+     <meta name="description" content="">
+    <meta name="author" content="">
+    
+    <title>ACE-Project</title>
+
+    <!-- Bootstrap -->
+    <link href="bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template--> 
+    <link href="signin.css" rel="stylesheet" type="text/css">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
---
+++ main/en/acp-ver2.0-doxgen-html/navtreedata.js
@@ -0,0 +1,56 @@
+var NAVTREE =
+[
+  [ "ACP Library", "index.html", [
+    [ "Introduction", "index.html#intro", [
+      [ "About This Document", "index.html#about", null ],
+      [ "Motivation", "index.html#motivation", null ],
+      [ "Approach", "index.html#approach", null ],
+      [ "Terminology", "index.html#terminology", null ]
+    ] ],
+    [ "Overview", "index.html#overview", [
+      [ "Fundamental Design", "index.html#fundamentalDesign", [
+        [ "Execution Model", "index.html#execModel", null ],
+        [ "Communication Models", "index.html#commModels", null ]
+      ] ],
+      [ "Interfaces", "index.html#interfaces", [
+        [ "Infrastructure", "index.html#infrastructure", null ],
+        [ "Channel", "index.html#channel", null ],
+        [ "Vector", "index.html#vector", null ],
+        [ "Deque", "index.html#deque", null ],
+        [ "List", "index.html#list", null ],
+        [ "Map", "index.html#map", null ],
+        [ "Malloc", "index.html#malloc", null ],
+        [ "Global Memory Management", "index.html#gmmSSSec", null ],
+        [ "Global Memory Access", "index.html#gmaSSSec", null ]
+      ] ]
+    ] ],
+    [ "Usage", "index.html#usage", [
+      [ "Installation", "index.html#installation", null ],
+      [ "Compilation", "index.html#compilation", null ],
+      [ "Execution", "index.html#execution", null ]
+    ] ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Structure Index", "classes.html", null ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"db/d7d/group__acpdl.html#ga6ab5fa90ca007033f04cfef96ee264c8"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
